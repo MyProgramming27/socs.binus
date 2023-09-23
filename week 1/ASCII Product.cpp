9ce7cee000dc60c956b7d291d674dd69307e05a6
MyProgramming27 <129528667+MyProgramming27@users.noreply.github.com>
2023-09-23 07:57:30 +0700

    Add files via upload

---
+++ week 1/ASCII Product.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main (){
+
+// char N [100];
+//  scanf("%[^\n]", N);
+  //  printf("Hello %s!\n", N);
+
+
+
+
+	char a,b;
+	scanf("%c %c", &a , &b);
+	int result= a*b;
+	printf("%d\n",result);
+	
+	return 0;
+}
